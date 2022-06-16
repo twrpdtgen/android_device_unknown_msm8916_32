21b872fcb12251fd40ae40d6a97785b39c89f269
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-16 19:26:57 +0200

    msm8916_32: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: unknown Model: msm8916_32 Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_msm8916_32.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_msm8916_32-user \
+    omni_msm8916_32-userdebug \
+    omni_msm8916_32-eng
---
+++ omni_msm8916_32.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from msm8916_32 device
+$(call inherit-product, device/unknown/msm8916_32/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := msm8916_32
+PRODUCT_NAME := omni_msm8916_32
+PRODUCT_BRAND := qcom
+PRODUCT_MODEL := msm8916_32
+PRODUCT_MANUFACTURER := unknown
